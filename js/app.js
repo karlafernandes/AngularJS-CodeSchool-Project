0f5851e0dcd524f1eaef9a96b7f72eab54307111
Karla Fernandes <karla@vitaminak.com.br>
2016-01-14 14:54:38 +0200

    feat: showing array of products

--- js/app.js
+++ js/app.js
@@ -1,12 +1,26 @@
 (function(){
 	var app = angular.module('store',[]);
 	app.controller('StoreController', function(){
-		this.product = gem;
+		this.products = gems;
 	});
 	
-	var gem = {
+	var gems = [
+	{
 		name: 'Dodecahedron',
 		price: 7.99,
-		description: 'bla bla bla',
-	}
+		description: 'Hummm 12 sides <3',
+		canPurchase: true,
+		soldOut: true
+	}, {
+		name: 'Pentagonal Gem',
+		price: 5.99,
+		description: '5 sides still good :)',
+		canPurchase: false,
+	}, {
+		name: 'Simple Gem',
+		price: 3.99,
+		description: 'Unique detail hun :)',
+		canPurchase: true,
+	}, 
+	]
 })();
