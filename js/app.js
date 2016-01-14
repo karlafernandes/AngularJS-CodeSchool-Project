68dd53912d071737f0e44051ff3e49fd99d9eb1c
Karla Fernandes <karla@vitaminak.com.br>
2016-01-14 19:23:29 +0200

    feat: adding image gallery

--- js/app.js
+++ js/app.js
@@ -6,21 +6,24 @@
 	
 	var gems = [
 	{
-		name: 'Dodecahedron',
+		name: "Dodecahedron",
 		price: 7.99,
-		description: 'Hummm 12 sides <3',
+		description: "Hummm 12 sides <3",
 		canPurchase: true,
-		soldOut: true
+		soldOut: true,
+		images: ['images/12.png', 'images/12min.png', 'images/12min1.png']
 	}, {
 		name: 'Pentagonal Gem',
-		price: 5.99,
+		price: 5.2,
 		description: '5 sides still good :)',
 		canPurchase: false,
+		images: ['images/5.png', 'images/5min.png', 'images/5min1.png']
 	}, {
 		name: 'Simple Gem',
-		price: 3.99,
+		price: 3,
 		description: 'Unique detail hun :)',
 		canPurchase: true,
+		images: ['images/1.png', 'images/1min.png', 'images/1min1.png']	
 	}, 
 	]
 })();
