0075dc9a1bcd47f677f332f2be6c15073ead9379
Karla Fernandes <karla@vitaminak.com.br>
2016-01-14 14:34:36 +0200

    feat: showing first product via js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,12 @@
-var app = angular.module('store',[]);
+(function(){
+	var app = angular.module('store',[]);
+	app.controller('StoreController', function(){
+		this.product = gem;
+	});
+	
+	var gem = {
+		name: 'Dodecahedron',
+		price: 7.99,
+		description: 'bla bla bla',
+	}
+})();
