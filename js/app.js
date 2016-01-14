92c2a9c0acd007b90542882b7462ba7957b8fec4
Karla Fernandes <karla@vitaminak.com.br>
2016-01-14 22:02:21 +0200

    feat: adding reviews + form

--- js/app.js
+++ js/app.js
@@ -22,6 +22,10 @@
 		};		
 	});	
 	
+	app.controller('ReviewController', function(){
+		this.review = {}; 
+	});
+	
 	var gems = [
 	{
 		name: "Dodecahedron",
@@ -29,19 +33,45 @@
 		description: "Hummm 12 sides <3",
 		canPurchase: true,
 		soldOut: true,
-		images: ['images/12.png', 'images/12min.png', 'images/12min1.png']
+		images: ['images/12.png', 'images/12min.png', 'images/12min1.png'],
+		reviews: [
+		{
+			stars: 5,
+			body: 'I love this product!',
+			author: 'me@google.com'	
+		},{
+			stars: 1,
+			body: 'I think there are better options on the market.',
+			author: 'she@google.com'	
+		},]
 	}, {
 		name: 'Pentagonal Gem',
 		price: 5.2,
 		description: '5 sides still good :)',
 		canPurchase: false,
-		images: ['images/5.png', 'images/5min.png', 'images/5min1.png']
+		images: ['images/5.png', 'images/5min.png', 'images/5min1.png'],
+		reviews: [
+		{
+			stars: 5,
+			body: '<3 I love this product!',
+			author: 'me@google.com'	
+		},{
+			stars: 1,
+			body: 'Maybe, there are better options on the market.',
+			author: 'she@google.com'	
+		},]		
 	}, {
 		name: 'Simple Gem',
 		price: 3,
 		description: 'Unique detail hun :)',
 		canPurchase: true,
-		images: ['images/1.png', 'images/1min.png', 'images/1min1.png']	
+		images: ['images/1.png', 'images/1min.png', 'images/1min1.png'],
+		reviews: [
+		{
+			stars: 5,
+			body: 'I want this product!',
+			author: 'me@google.com'	
+		},]		
 	}, 
 	]
 })();
