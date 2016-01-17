cfb8921f6ff5dd3cee4ecfb3a2fbea7c94c0dc7b
Karla Fernandes <karla@vitaminak.com.br>
2016-01-17 19:50:33 +0200

    fix: json

--- js/app.js
+++ js/app.js
@@ -1,38 +1,22 @@
-(function(){
-	var app = angular.module('store',['store-products']);
-	/*
-	$http({method:'GET',url:'js/products.json'});
-	$http.get('js/products.json',{apiKey:''myApiKey});
-	.sucess() / .error()
+(function() {
+  var app = angular.module('store', ['store-products']);
 
-	app.controller('StoreController', ['$http','$log', function($http,$log){
-		this.products = products.json;	
-	} ]);	
-	*/
-	app.controller('StoreController', ['$http', function($http){
-		var store = this;	
-		store.products = [ ];
-		
-		$http.get('js/products.json').success(function(data){
-		store.products = data;
+  app.controller('StoreController', ['$http', function($http){
+    	var store = this;
+    	store.products = [];
+
+    	$http.get('js/products.json').success(function(data){
+			store.products = data;
 		});
 	}]);
-	
-	app.controller('GalleryController', function(){	
-		this.current = 0;
-		this.setCurrent = function(imageNumber){
-			this.current = imageNumber || 0;
-		};
-	});	
-	
-	app.controller('ReviewController', function(){
-		this.review = {}; 
-		
-		this.addReview = function(product){
-			this.review.createdOn = Date.now();
-			product.reviews.push(this.review);
-			this.review = {}; 
-		};
-	});
-	
+
+  app.controller('ReviewController', function() {
+    this.review = {};
+
+    this.addReview = function(product) {
+      product.reviews.push(this.review);
+
+      this.review = {};
+    };
+  });
 })();
--- js/products.js
+++ js/products.js
@@ -1,33 +1,64 @@
 (function(){
-	var app = angular.module('store-products', [ ]);
-	app.directive('productTitle', function() {
-		return {
-			restrict: 'E', // type of directive Element
-			templateUrl: 'product-title.html'
-		};
-	});
+    var app = angular.module('store-products', []);
 
-	app.directive('productPanel', function() {
-		return {
-			restrict: 'E',
-			templateUrl: 'product-panels.html',
-			controller: function(){
-				this.tab = 1; 
-				this.selectTab = function(setTab){
-					this.tab = setTab;
-				};
-				this.isSelected = function(checkTab){
-					return this.tab === checkTab;
-				};		
-			},
-			controllerAs: 'panels'
-		};		
-	});
+    app.directive("productTitle", function() {
+      return {
+        restrict: 'E',
+        templateUrl: "product-title.html"
+      };
+    });
 
-	app.directive("productSpecs", function() {
-		return {
-			restrict: 'A', // type of directive Attribute
-			templateUrl: "product-specs.html"
-		};
-	});
-})();
+    app.directive("productDescription", function() {
+      return {
+        restrict: 'E',
+        templateUrl: "product-description.html"
+      };
+    });
+
+    app.directive("productReviews", function() {
+      return {
+        restrict: 'E',
+        templateUrl: "product-reviews.html"
+      };
+    });
+
+    app.directive("productSpecs", function() {
+      return {
+        restrict:"A",
+        templateUrl: "product-specs.html"
+      };
+    });
+
+    app.directive("productTabs", function() {
+      return {
+        restrict: "E",
+        templateUrl: "product-tabs.html",
+        controller: function() {
+          this.tab = 1;
+
+          this.isSet = function(checkTab) {
+            return this.tab === checkTab;
+          };
+
+          this.setTab = function(activeTab) {
+            this.tab = activeTab;
+          };
+        },
+        controllerAs: "tab"
+      };
+    });
+
+    app.directive("productGallery", function() {
+      return {
+        restrict: "E",
+        templateUrl: "product-gallery.html",
+        controller: function() {
+          this.current = 0;
+          this.setCurrent = function(imageNumber){
+            this.current = imageNumber || 0;
+          };
+        },
+        controllerAs: "gallery"
+      };
+    });
+  })();
